a28f4056a7f72ede046f45638f06064a80292ab9
DeviTobi <DeviTobi@users.noreply.github.com>
2019-04-26 17:35:27 +0200

    MapGenerator feat start

---
+++ DoUEvenRift/Assets/Scripts/MapGenerator/BitmapReader.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BitmapReader : MonoBehaviour {
+	public Texture2D tex;
+
+	private Color[,] pixels;
+
+	void Start () {
+		if (tex != null){
+			pixels = new Color[tex.width, tex.height];
+
+			Color[] pixelTemp;
+			pixelTemp = tex.GetPixels();
+
+			for (int y = 0; y < tex.height; y++) {
+				for (int x = 0; x < tex.width; x++) {
+					pixels[x,y] = pixelTemp[x*(y * tex.width)];
+					Debug.Log(x+(y * tex.width));
+				}
+			}
+		}
+	}
+
+	void Update () {
+		
+	}
+}
